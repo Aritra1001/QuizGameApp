a5af2a9c1c046e90c8f726b3c13ec1834649f084
Aritra Nandi <aritranandi30@gmail.com>
2025-11-13 19:14:40 +0530

    rendering the question and option

--- quiz.js
+++ quiz.js
@@ -1,9 +1,21 @@
-const questionObj = 
-    {
-      category: 'Food & Drink',
-      id: 'qa-1',
-      correctAnswer: 'Three',
-      answers: ['Two', 'Three ', 'Four', 'Five'],
-      question:
-        "How many pieces of bun are in a Mcdonald's Big Mac?",
-    };
+const questionObj = {
+  category: "Food & Drink",
+  id: "qa-1",
+  correctAnswer: "Three",
+  options: ["Two", "Three ", "Four", "Five"],
+  question: "How many pieces of bun are in a Mcdonald's Big Mac?",
+};
+
+const questionEl = document.getElementById("question");
+const optionEl = document.getElementById("options");
+
+// rendering the question.
+// console.log("questionDiv", questionDiv);
+questionEl.textContent = questionObj.question;
+
+//rendering the options
+questionObj.options.forEach((_option) => {
+  const btnEl = document.createElement("button");
+  btnEl.textContent = _option;
+  optionEl.appendChild(btnEl);
+});
